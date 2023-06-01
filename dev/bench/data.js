1fe628b37a7bb8c22740fb2ef42fa4510b81ad88
github-action-benchmark <github@users.noreply.github.com>
2023-06-01 13:04:00 +0000

    add Benchmark (pytest) benchmark result for 3edca12f551cc3b2fd231b8a578819ad3bcafe27

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685618523981,
+  "lastUpdate": 1685624640755,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -19336,6 +19336,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005641368380136675",
             "extra": "mean: 228.812837600708 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ferran@nuclia.com",
+            "name": "Ferran Llamas",
+            "username": "lferran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3edca12f551cc3b2fd231b8a578819ad3bcafe27",
+          "message": "Improve iterating over resource fields (#947)\n\n* Add proto for all fields key value\n\n* Add allfields tikv key\n\n* Improve how generic fields are added\n\n* Rename VALID_GLOBAL -> VALID_GENERIC_FIELDS to be more explicit\n\n* Add get and set field ids\n\n* Properly update all fields key\n\n* Add tests and fix typing\n\n* Fix delete field: remove from all fields keys\n\n* Fix typing\n\n* Fix more mypy",
+          "timestamp": "2023-06-01T12:58:07Z",
+          "tree_id": "5ab98c327ae40f04da2ad4a8c7247d53636f115f",
+          "url": "https://github.com/nuclia/nucliadb/commit/3edca12f551cc3b2fd231b8a578819ad3bcafe27"
+        },
+        "date": 1685624639379,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 4404.263152135997,
+            "unit": "iter/sec",
+            "range": "stddev: 9.860912881600407e-8",
+            "extra": "mean: 227.0527362823486 usec\nrounds: 5"
           }
         ]
       }
