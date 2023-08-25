e5dc42a5e7f8f311227af9fcfd01ec7266af1e2a
github-action-benchmark <github@users.noreply.github.com>
2023-08-25 16:16:41 +0000

    add Benchmark (pytest) benchmark result for 7a58996f1b6348732ab2829493b2b9e0ab253b0b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1692971301078,
+  "lastUpdate": 1692980201746,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -35120,6 +35120,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000447449006182895",
             "extra": "mean: 6.0715066062079535 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "44120086+hermeGarcia@users.noreply.github.com",
+            "name": "Herme",
+            "username": "hermeGarcia"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "7a58996f1b6348732ab2829493b2b9e0ab253b0b",
+          "message": "add telemtry at the start (#1256)",
+          "timestamp": "2023-08-25T16:05:34Z",
+          "tree_id": "f6969a60242e840d55978b70e591d87dc767b8d2",
+          "url": "https://github.com/nuclia/nucliadb/commit/7a58996f1b6348732ab2829493b2b9e0ab253b0b"
+        },
+        "date": 1692980199250,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/tests/benchmarks/test_search.py::test_search_returns_labels",
+            "value": 79.50165891294195,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007998968703162909",
+            "extra": "mean: 12.578353881835938 msec\nrounds: 5"
+          },
+          {
+            "name": "nucliadb/tests/benchmarks/test_search.py::test_search_relations",
+            "value": 201.8938329129522,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00013800343480736854",
+            "extra": "mean: 4.953098297119141 msec\nrounds: 5"
           }
         ]
       }
