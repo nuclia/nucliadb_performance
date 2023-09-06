2e8aaf5b1408c8e15c0c75011518db8d194ea114
github-action-benchmark <github@users.noreply.github.com>
2023-09-06 07:47:24 +0000

    add Benchmark (pytest) benchmark result for 47a7a4b1333ae5de15eb070c09e23f809682d764

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1693929959893,
+  "lastUpdate": 1693986443973,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -37434,6 +37434,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00018367051174740328",
             "extra": "mean: 4.755434122952548 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ferran@nuclia.com",
+            "name": "Ferran Llamas",
+            "username": "lferran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "47a7a4b1333ae5de15eb070c09e23f809682d764",
+          "message": "Deduplicate vector results by default in chat (#1299)\n\n* Be able to deduplicate vector results\n\n* Add test",
+          "timestamp": "2023-09-06T07:35:12Z",
+          "tree_id": "e8c5f8f46dd6b504813cd946eb18d976a3180985",
+          "url": "https://github.com/nuclia/nucliadb/commit/47a7a4b1333ae5de15eb070c09e23f809682d764"
+        },
+        "date": 1693986441595,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/tests/benchmarks/test_search.py::test_search_returns_labels",
+            "value": 61.385747947370724,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0007087239194166256",
+            "extra": "mean: 16.29042625427246 msec\nrounds: 5"
+          },
+          {
+            "name": "nucliadb/tests/benchmarks/test_search.py::test_search_relations",
+            "value": 163.2703254573128,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00017379224549446233",
+            "extra": "mean: 6.124811702304416 msec\nrounds: 5"
           }
         ]
       }
