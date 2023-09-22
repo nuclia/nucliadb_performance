5b44d159507063157cb207d14ff0f55e6f024ab7
github-action-benchmark <github@users.noreply.github.com>
2023-09-22 15:51:33 +0000

    add Benchmark (pytest) benchmark result for 86d97bb41770b53668423ee54ff5c9ea50365625

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695387131350,
+  "lastUpdate": 1695397893392,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -40509,6 +40509,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000841602781715384",
             "extra": "mean: 5.047729015350342 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vangheem@gmail.com",
+            "name": "vangheem",
+            "username": "vangheem"
+          },
+          "committer": {
+            "email": "vangheem@gmail.com",
+            "name": "vangheem",
+            "username": "vangheem"
+          },
+          "distinct": true,
+          "id": "86d97bb41770b53668423ee54ff5c9ea50365625",
+          "message": "add locks on connection use",
+          "timestamp": "2023-09-22T11:46:37-04:00",
+          "tree_id": "04863e1d01b1a50148fd9a752e81b83d1a8f1bbf",
+          "url": "https://github.com/nuclia/nucliadb/commit/86d97bb41770b53668423ee54ff5c9ea50365625"
+        },
+        "date": 1695397891120,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 8711.146926085232,
+            "unit": "iter/sec",
+            "range": "stddev: 4.7069521919905176e-7",
+            "extra": "mean: 114.79544639587402 usec\nrounds: 5"
           }
         ]
       }
