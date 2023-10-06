487bdb59d1c4e964549ef539106689fc72d00896
github-action-benchmark <github@users.noreply.github.com>
2023-10-06 08:02:52 +0000

    add Benchmark (pytest) benchmark result for 173408facefab42c41f0eb2e5fcfd3a05c137898

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1696579219028,
+  "lastUpdate": 1696579372287,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -46199,6 +46199,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000943941601339384",
             "extra": "mean: 15.324158668518066 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ferran@nuclia.com",
+            "name": "Ferran Llamas",
+            "username": "lferran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "173408facefab42c41f0eb2e5fcfd3a05c137898",
+          "message": "Small fixes on Import/Export (#1425)\n\n* Done\n\n* Done\n\n* Be careful not to open a txn when one is already open - pg driver does not like it\n\n* Be careful not to open a txn when one is already open - pg driver does not like it",
+          "timestamp": "2023-10-06T07:52:55Z",
+          "tree_id": "465c751f9671d50114a4e421fa5b6ae17d262efa",
+          "url": "https://github.com/nuclia/nucliadb/commit/173408facefab42c41f0eb2e5fcfd3a05c137898"
+        },
+        "date": 1696579367755,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/tests/benchmarks/test_search.py::test_search_returns_labels[tikv_driver_settings]",
+            "value": 38.915492513439446,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0016195911465922285",
+            "extra": "mean: 25.696706771850586 msec\nrounds: 5"
+          },
+          {
+            "name": "nucliadb/tests/benchmarks/test_search.py::test_search_relations[tikv_driver_settings]",
+            "value": 126.78321805150243,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008974287871403129",
+            "extra": "mean: 7.88747923714774 msec\nrounds: 5"
           }
         ]
       }
