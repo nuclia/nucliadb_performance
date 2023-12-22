e9c66d01cc3b581072e23d8824049a6337016c3b
github-action-benchmark <github@users.noreply.github.com>
2023-12-22 18:49:37 +0000

    add Benchmark (pytest) benchmark result for 0a5a6d78eb2c1ce8f47d775032e2708a62d92a02

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1703270974700,
+  "lastUpdate": 1703270977908,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -51664,6 +51664,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 2.0491197332825977e-7",
             "extra": "mean: 78.32010806594764 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "distinct": true,
+          "id": "0a5a6d78eb2c1ce8f47d775032e2708a62d92a02",
+          "message": "chore: autopublish 2023-12-22T18:42:47Z",
+          "timestamp": "2023-12-22T18:42:47Z",
+          "tree_id": "b24be67c4a7434330d8dd4e233162ce261648aea",
+          "url": "https://github.com/nuclia/nucliadb/commit/0a5a6d78eb2c1ce8f47d775032e2708a62d92a02"
+        },
+        "date": 1703270975536,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 12698.543206884015,
+            "unit": "iter/sec",
+            "range": "stddev: 2.6103173481161066e-7",
+            "extra": "mean: 78.74919065187646 usec\nrounds: 5"
           }
         ]
       }
