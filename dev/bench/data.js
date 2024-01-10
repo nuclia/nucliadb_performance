11a1fc6fd3ab5b7bc80a265734a8a9a51f3affc1
github-action-benchmark <github@users.noreply.github.com>
2024-01-10 14:17:22 +0000

    add Benchmark (pytest) benchmark result for 12509a9a1530ed010fb0337e1149fc76680e8c91

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704895846772,
+  "lastUpdate": 1704896241989,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -52594,6 +52594,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 8.208418821972303e-7",
             "extra": "mean: 76.36155492026356 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ferran@nuclia.com",
+            "name": "Ferran Llamas",
+            "username": "lferran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "12509a9a1530ed010fb0337e1149fc76680e8c91",
+          "message": "Rename activity to notifications (#1713)\n\n* rename activity -> notifications\r\n\r\n* rename activity -> notifications",
+          "timestamp": "2024-01-10T15:03:55+01:00",
+          "tree_id": "644bb2a7ae00e0d536d66590ceee8c004f51b346",
+          "url": "https://github.com/nuclia/nucliadb/commit/12509a9a1530ed010fb0337e1149fc76680e8c91"
+        },
+        "date": 1704896239630,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 13280.466989137647,
+            "unit": "iter/sec",
+            "range": "stddev: 2.3213526085044297e-7",
+            "extra": "mean: 75.2985569572154 usec\nrounds: 5"
           }
         ]
       }
