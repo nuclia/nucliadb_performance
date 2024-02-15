dad61e8bb18ce9b6ce4b138b9c7556598e269bf2
github-action-benchmark <github@users.noreply.github.com>
2024-02-15 10:51:08 +0000

    add Benchmark (pytest) benchmark result for e590bb4c2a39e629700c5a9c00ac76ef5c807845

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707988635204,
+  "lastUpdate": 1707994268904,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -57771,6 +57771,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 3.2623079558868044e-7",
             "extra": "mean: 76.2728572254123 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joanantoni.re16@gmail.com",
+            "name": "Joan Antoni RE",
+            "username": "jotare"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e590bb4c2a39e629700c5a9c00ac76ef5c807845",
+          "message": "Fix error code parsing for AWS S3 responses (#1837)\n\n* Upgrade fake S3 docker image\r\n\r\n* Fix error code parsing for AWS S3 responses\r\n\r\n* Add unit tests to s3 parse_status_code\r\n\r\n* Revert \"Upgrade fake S3 docker image\"\r\n\r\nThis reverts commit ef109f07b67f8bcc4e13d389e488c0e66030d659.\r\n\r\n* Mypy...\r\n\r\n* Add mypy to pre-commit",
+          "timestamp": "2024-02-15T11:46:24+01:00",
+          "tree_id": "f7ad50b9287d46f16051a51994645c056c88a838",
+          "url": "https://github.com/nuclia/nucliadb/commit/e590bb4c2a39e629700c5a9c00ac76ef5c807845"
+        },
+        "date": 1707994266008,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 13175.051264674483,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000016327617172451343",
+            "extra": "mean: 75.90103293800786 usec\nrounds: 5"
           }
         ]
       }
