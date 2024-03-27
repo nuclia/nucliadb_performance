b1c88af2da0f90b7064cbfab462be1f09f1ec4ac
github-action-benchmark <github@users.noreply.github.com>
2024-03-27 12:30:58 +0000

    add Benchmark (pytest) benchmark result for 222974c6dedf981f55395a5f84cbbead9dde4d5a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711542194471,
+  "lastUpdate": 1711542658835,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -65459,6 +65459,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 2.1674367835642143e-7",
             "extra": "mean: 75.01417819452108 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "joanantoni.re16@gmail.com",
+            "name": "Joan Antoni RE",
+            "username": "jotare"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "222974c6dedf981f55395a5f84cbbead9dde4d5a",
+          "message": "Add more context to errors on get active shards (#2009)",
+          "timestamp": "2024-03-27T13:17:33+01:00",
+          "tree_id": "f8d385129ef1929364346f433cf8f82c5438462d",
+          "url": "https://github.com/nuclia/nucliadb/commit/222974c6dedf981f55395a5f84cbbead9dde4d5a"
+        },
+        "date": 1711542655211,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 13291.894056373656,
+            "unit": "iter/sec",
+            "range": "stddev: 1.6648885073844797e-7",
+            "extra": "mean: 75.23382264098663 usec\nrounds: 5"
           }
         ]
       }
