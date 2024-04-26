8a6a987075b7fa7963af24dbce1330239335f719
github-action-benchmark <github@users.noreply.github.com>
2024-04-26 09:26:46 +0000

    add Benchmark (pytest) benchmark result for 7ce752c5e71bfce3da611ae4bfb75a4cb78f1a06

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714120473280,
+  "lastUpdate": 1714123606127,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -70326,6 +70326,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 3.3977274207586405e-7",
             "extra": "mean: 129.36663627624515 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "44120086+hermeGarcia@users.noreply.github.com",
+            "name": "Herme",
+            "username": "hermeGarcia"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7ce752c5e71bfce3da611ae4bfb75a4cb78f1a06",
+          "message": "old version names (#2096)",
+          "timestamp": "2024-04-26T11:08:09+02:00",
+          "tree_id": "22094cef71bbe63d5261dc21e5da3cb793cc948b",
+          "url": "https://github.com/nuclia/nucliadb/commit/7ce752c5e71bfce3da611ae4bfb75a4cb78f1a06"
+        },
+        "date": 1714123602016,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 7733.796712876779,
+            "unit": "iter/sec",
+            "range": "stddev: 3.511445798112833e-7",
+            "extra": "mean: 129.30259704589844 usec\nrounds: 5"
           }
         ]
       }
