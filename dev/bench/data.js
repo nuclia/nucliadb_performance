6c673c895eb00ebbd45ac40fd2265a8a3153e577
github-action-benchmark <github@users.noreply.github.com>
2024-05-07 12:31:33 +0000

    add Benchmark (pytest) benchmark result for 3467cd831f719db4738ab0572d52f13c60a35c00

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715084921023,
+  "lastUpdate": 1715085093712,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -72062,6 +72062,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 4.0843005936527773e-7",
             "extra": "mean: 75.14372456714477 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ramon@nuclia.com",
+            "name": "Ramon Navarro Bosch",
+            "username": "bloodbare"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3467cd831f719db4738ab0572d52f13c60a35c00",
+          "message": "Add generated files on links (#2125)\n\n* Add generated files on links\r\n\r\n* FIX",
+          "timestamp": "2024-05-07T14:23:28+02:00",
+          "tree_id": "00c83a3c9eb30b8630d0644d08add0dc36165280",
+          "url": "https://github.com/nuclia/nucliadb/commit/3467cd831f719db4738ab0572d52f13c60a35c00"
+        },
+        "date": 1715085088841,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 13158.377684939582,
+            "unit": "iter/sec",
+            "range": "stddev: 2.2799858107548344e-7",
+            "extra": "mean: 75.99721059417148 usec\nrounds: 5"
           }
         ]
       }
