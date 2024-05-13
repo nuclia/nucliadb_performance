5c3c1cc9a7a30dc01231a01885e214c5f13794b5
github-action-benchmark <github@users.noreply.github.com>
2024-05-13 13:40:29 +0000

    add Benchmark (pytest) benchmark result for 169a3e9f1d823efb51563b43c6896e89745225a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715607493714,
+  "lastUpdate": 1715607629513,
   "repoUrl": "https://github.com/nuclia/nucliadb",
   "entries": {
     "Benchmark": [
@@ -73457,6 +73457,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 3.7372300946332225e-7",
             "extra": "mean: 75.36008197127914 usec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "javier@javiertorres.eu",
+            "name": "Javier Torres",
+            "username": "javitonino"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "169a3e9f1d823efb51563b43c6896e89745225a8",
+          "message": "Increase maximum message size for index writer grpc service (#2140)",
+          "timestamp": "2024-05-13T15:30:47+02:00",
+          "tree_id": "0ff30b0adb25e4768890e89a512f249d78d79c25",
+          "url": "https://github.com/nuclia/nucliadb/commit/169a3e9f1d823efb51563b43c6896e89745225a8"
+        },
+        "date": 1715607624957,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "nucliadb/search/tests/unit/search/test_fetch.py::test_highligh_error",
+            "value": 13347.118184568606,
+            "unit": "iter/sec",
+            "range": "stddev: 1.4284820519824024e-7",
+            "extra": "mean: 74.92254029459028 usec\nrounds: 5"
           }
         ]
       }
